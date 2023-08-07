c10a93e479788146eb80716d37a526ee96b865a0
Mattool81974 <mmattmmo@gmail.com>
2023-08-07 17:16:27 +0200

    Update hierarchy system

--- mlib.py
+++ mlib.py
@@ -3,18 +3,29 @@ import pygame
 
 pygame.init()
 
+#Nombre de widget existants
+_nbWidget = 0
+
 class MWidget:
     def __init__(self, x, y, width, height, parent):
         self.height = height
-        self.parent = parent
         self.shouldModify = True
         self.width = width
         self.x = x
         self.y = y
+        self._children = []
+        self._id = _nbWidget
         self._lastSurface = 0
+
+        _nbWidget += 1
+
+        self.setParent(parent)
 
     def getHeight(self):
         return self.height
+    
+    def getID(self):
+        return self._id
 
     def getParent(self):
         return self.parent
@@ -44,6 +55,8 @@ class MWidget:
         self.parent = newParent
         self.shouldModify = True
 
+        newParent._addChild(self)
+
     def setWidth(self, newWidth):
         self.width = newWidth
         self.shouldModify = True
@@ -55,6 +68,16 @@ class MWidget:
     def setY(self, newY):
         self.y = newY
         self.shouldModify = True
+
+    def _addChild(self, child):
+        if not self._containsChild(child.getId()):
+            self._children.append(child)
+
+    def _containsChild(self, id):
+        for i in self._children:
+            if i.getID() == id:
+                return True
+        return False
 
     def _render(self, surface):
 
