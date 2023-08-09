fe3dd5f1fb8d7433566b82808fc79b6c0b5671f2
Mattool81974 <mmattmmo@gmail.com>
2023-08-09 16:53:10 +0200

    Upgrade hierarchy system and add event system

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import pygame
 TAILLE = (700, 394)
 
 fenetre = display.set_mode(TAILLE)
-mapp = MApp(fenetre)
+mapp = MApp(fenetre, TAILLE[0], TAILLE[1])
+widget = MWidget(0, 0, 100, 100, mapp)
 
 while True:
     mapp.frame()
 
     pygame.display.flip()
-
-pygame.quit()
--- mlib.py
+++ mlib.py
@@ -8,7 +8,9 @@ _nbWidget = 0
 
 class MWidget:
     def __init__(self, x, y, width, height, parent):
+        global _nbWidget
         self.height = height
+        self.parent = 0
         self.shouldModify = True
         self.width = width
         self.x = x
@@ -19,7 +21,14 @@ class MWidget:
 
         _nbWidget += 1
 
-        self.setParent(parent)
+        if parent != 0:
+            self.setParent(parent)
+
+    def containsChild(self, id):
+        for i1, i2 in enumerate(self._children):
+            if i2.getID() == id:
+                return i1
+        return -1
 
     def getHeight(self):
         return self.height
@@ -52,7 +61,11 @@ class MWidget:
         self.shouldModify = True
     
     def setParent(self, newParent):
+        if self.parent != 0:
+            self.parent._removeChild(self)
+
         self.parent = newParent
+        self.parent.shouldModify = True
         self.shouldModify = True
 
         newParent._addChild(self)
@@ -70,14 +83,13 @@ class MWidget:
         self.shouldModify = True
 
     def _addChild(self, child):
-        if not self._containsChild(child.getId()):
+        if self.containsChild(child.getID()) == -1:
             self._children.append(child)
-
-    def _containsChild(self, id):
-        for i in self._children:
-            if i.getID() == id:
-                return True
-        return False
+    
+    def _removeChild(self, child):
+        id = self.containsChild(child.getId())
+        if id != -1:
+            self._children = self._children[:id] + self._children[id:]
 
     def _render(self, surface):
 
@@ -96,8 +108,16 @@ class MWidget:
         return surface
 
 class MApp(MWidget):
-    def __init__(self, pygameWindow):
+    def __init__(self, pygameWindow, windowWidth, windowHeight):
+        MWidget.__init__(self, 0, 0, windowWidth, windowHeight, 0)
         self._pygameWindow = pygameWindow
 
     def frame(self):
-        pass
+        self.frameEvent()
+
+    def frameEvent(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
