99de492212a97ae220c50176d1da3cb4a55bbde0
Mattool81974 <mmattmmo@gmail.com>
2023-08-12 13:24:32 +0200

    Add windowTitle, deltaTime and fps

--- main.py
+++ main.py
@@ -1,13 +1,109 @@
 from mlib import *
+from math import *
 import pygame
+from random import randint
+from time import sleep
 
-TAILLE = (700, 394)
+#TAILLE = (700, 394)
+TAILLE = (750, 750)
 
 fenetre = display.set_mode(TAILLE)
-mapp = MApp(fenetre, TAILLE[0], TAILLE[1])
-widget = MWidget(0, 0, 100, 100, mapp)
+mapp = MApp(fenetre, "Test", TAILLE[0], TAILLE[1], backgroundColor=(255, 255, 255))
+
+carreTaille = (25, 25)
+widgets = []
+for i in range(carreTaille[0]):
+    widgetsSub = []
+    for j in range(carreTaille[1]):
+        color = (0, 0, 0)
+        widget = MWidget((TAILLE[0] / carreTaille[0]) * i, (TAILLE[1] / carreTaille[1]) * j, (TAILLE[0] / carreTaille[0]), (TAILLE[1] / carreTaille[1]), mapp, backgroundColor=(color))
+        widgetsSub.append(widget)
+    widgets.append(widgetsSub)
+
+def carreCentral(color = (0, 0, 0)):
+    widgets[floor(carreTaille[0]/2)][floor(carreTaille[1]/2)].setBackgroundColor(color)
+
+def carreExterieur(color = (0, 0, 0)):
+    for i in range(carreTaille[1]):
+        widgets[0][i].setBackgroundColor(color)
+
+    for i in range(carreTaille[1]):
+        widgets[carreTaille[0] - 1][i].setBackgroundColor(color)
+
+    for i in range(carreTaille[0]):
+        widgets[i][0].setBackgroundColor(color)
+
+    for i in range(carreTaille[0]):
+        widgets[i][carreTaille[1] - 1].setBackgroundColor(color)
+
+def carreInterne(color = (0, 0, 0), n = 0):
+    n2 = ceil(carreTaille[0]/2) - (ceil(carreTaille[0]/2) - n)
+
+    ns = 0
+    if carreTaille[0]%2 == 1:
+        ns = 1
+
+    for i in range(n2):
+        ns += 2
+    
+    for i in range(ns):
+        nb = floor(carreTaille[0]/2) - floor(ns/2)
+        widgets[nb][i + nb].setBackgroundColor(color)
+
+    for i in range(ns):
+        nb = floor(carreTaille[0]/2) - floor(ns/2)
+        nb2 = floor(carreTaille[0]/2) + floor(ns/2)
+        widgets[nb2][i + nb].setBackgroundColor(color)
+
+    for i in range(ns):
+        nb = floor(carreTaille[0]/2) - floor(ns/2)
+        widgets[i + nb][nb].setBackgroundColor(color)
+
+    for i in range(ns):
+        nb = floor(carreTaille[0]/2) - floor(ns/2)
+        nb2 = floor(carreTaille[0]/2) + floor(ns/2)
+        widgets[i + nb][nb2].setBackgroundColor(color)
+
+t = True
+
+limite = 0.1
+temps = 0
 
 while True:
-    mapp.frame()
+    mapp.frameEvent()
 
+    temps += mapp.getDeltaTime()
+
+    if temps > limite:
+        temps -= limite
+
+        n = ceil(carreTaille[0]/2) - 1
+
+        if t:
+            t = False
+            carreCentral((255, 0, 0))
+
+            o = False
+            for i in range(n):
+                if o:
+                    o = False
+                    carreInterne((255, 0, 0), 1 + i)
+                else:
+                    o = True
+                    carreInterne((0, 0, 0), 1 + i)
+        else:
+            t = True
+            carreCentral((0, 0, 0))
+            carreExterieur((0, 0, 0))
+
+            o = True
+            for i in range(n):
+                if o:
+                    o = False
+                    carreInterne((255, 0, 0), 1 + i)
+                else:
+                    o = True
+                    carreInterne((0, 0, 0), 1 + i)
+
+    mapp.frameGraphics()
     pygame.display.flip()
--- mlib.py
+++ mlib.py
@@ -1,17 +1,20 @@
 from pygame import *
 import pygame
+from time import time_ns
 
 pygame.init()
 
 #Nombre de widget existants
 _nbWidget = 0
 
+###################### Base widget class
 class MWidget:
-    def __init__(self, x, y, width, height, parent):
+    def __init__(self, x, y, width, height, parent, backgroundColor = (0, 0, 0)): #MWidget's constructor
         global _nbWidget
+        self.backgroundColor = backgroundColor
         self.height = height
         self.parent = 0
-        self.shouldModify = True
+        self.setShouldModify(True)
         self.width = width
         self.x = x
         self.y = y
@@ -24,100 +27,177 @@ class MWidget:
         if parent != 0:
             self.setParent(parent)
 
-    def containsChild(self, id):
-        for i1, i2 in enumerate(self._children):
+    def containsChild(self, id): #Return if the widget contains the child with the id
+        for i1, i2 in enumerate(self._children): #Search through all the children to find the one with the good id
             if i2.getID() == id:
                 return i1
         return -1
+    
+    def getBackgroundColor(self): #Return the value of backgroundColor
+        return self.backgroundColor
 
-    def getHeight(self):
+    def getHeight(self): #Return the value of height
         return self.height
     
-    def getID(self):
+    def getID(self): #Return the value of _id
         return self._id
 
-    def getParent(self):
+    def getParent(self): #Return the value of parent
         return self.parent
     
-    def getWidth(self):
+    def getRect(self): #Return (x, y, width, height)
+        return (self.getX(), self.getY(), self.getWidth(), self.getHeight())
+    
+    def getShouldModify(self): #Return the value of should modify
+        return self.shouldModify
+    
+    def getWidth(self): #Return the value of width
         return self.width
     
-    def getX(self):
+    def getX(self): #Return the value of X
         return self.x
     
-    def getY(self):
+    def getY(self): #Return the value of Y
         return self.y
     
-    def move(self, newX, newY):
+    def move(self, newX, newY): #Change the value of X and Y in one function
         self.setX(newX)
         self.setY(newY)
 
-    def resize(self, newWidth, newHeight):
+    def resize(self, newWidth, newHeight): #Change the value of width and height in one function
         self.setHeight(newHeight)
         self.setWidth(newWidth)
+
+    def setBackgroundColor(self, backgroundColor): #Change the value of backgroundColor
+        self.backgroundColor = backgroundColor
+        self.setShouldModify(True)
     
-    def setHeight(self, newHeight):
+    def setHeight(self, newHeight): #Change the value of height
         self.height = newHeight
-        self.shouldModify = True
+        self.setShouldModify(True)
     
-    def setParent(self, newParent):
+    def setParent(self, newParent): #Change the value of parent
         if self.parent != 0:
             self.parent._removeChild(self)
 
         self.parent = newParent
-        self.parent.shouldModify = True
-        self.shouldModify = True
+        self.setShouldModify(True)
 
         newParent._addChild(self)
 
-    def setWidth(self, newWidth):
+    def setShouldModify(self, shouldModify): #Change the value of shouldModify
+        if shouldModify:
+            self.shouldModify = True
+            if self.parent != 0:
+                self.parent.setShouldModify(True)
+        else:
+            self.shouldModify = False
+
+    def setWidth(self, newWidth): #Change the value of width
         self.width = newWidth
-        self.shouldModify = True
+        self.setShouldModify(True)
 
-    def setX(self, newX):
+    def setX(self, newX): #Change the value of X
         self.x = newX
-        self.shouldModify = True
+        self.setShouldModify(True)
 
-    def setY(self, newY):
+    def setY(self, newY): #Change the value of Y
         self.y = newY
-        self.shouldModify = True
+        self.setShouldModify(True)
 
-    def _addChild(self, child):
+    def _addChild(self, child): #Add a child to the widget
         if self.containsChild(child.getID()) == -1:
             self._children.append(child)
     
-    def _removeChild(self, child):
+    def _removeChild(self, child): #Remove a child to the widget
         id = self.containsChild(child.getId())
         if id != -1:
             self._children = self._children[:id] + self._children[id:]
 
-    def _render(self, surface):
+    def _render(self): #Render widget and return rendering
 
-        widgetSurface = Surface((self.width, self.height))
-        widgetSurface.blit(self._renderBeforeHierarchy(surface))
-        widgetSurface.blit(self._renderAfterHierarchy(surface))
+        if self.getShouldModify(): #If the widget should do the render, do it
+            widgetSurface = Surface((self.width, self.height), pygame.SRCALPHA)
+            widgetSurface.fill(self.backgroundColor, (0, 0, self.getWidth(), self.getHeight()))
 
-        surface.blit(widgetSurface)
+            widgetSurface.blit(self._renderBeforeHierarchy(widgetSurface), (0, 0, self.getWidth(), self.getHeight()))
+            widgetSurface.blit(self._renderHierarchy(widgetSurface), (0, 0, self.getWidth(), self.getHeight()))
+            widgetSurface.blit(self._renderAfterHierarchy(widgetSurface), (0, 0, self.getWidth(), self.getHeight()))
 
+            self._lastSurface = widgetSurface #Store the last generated surface into _lastSurface
+
+            self.setShouldModify(False) #Update shouldModify
+            return widgetSurface
+        #Else, use _lastSurface
+        return self._lastSurface
+    
+    def _renderAfterHierarchy(self, surface): #Render widget on surface after hierarchy render
         return surface
     
-    def _renderAfterHierarchy(self, surface):
+    def _renderBeforeHierarchy(self, surface): #Render widget on surface before hierarchy render
         return surface
     
-    def _renderBeforeHierarchy(self, surface):
+    def _renderHierarchy(self, surface): #Render hierarchy on surface
+        for child in self._children: #Get the _render of all the children
+            surface.blit(child._render(), child.getRect())
         return surface
 
+###################### Main application class
 class MApp(MWidget):
-    def __init__(self, pygameWindow, windowWidth, windowHeight):
-        MWidget.__init__(self, 0, 0, windowWidth, windowHeight, 0)
+    def __init__(self, pygameWindow, windowTitle, windowWidth, windowHeight, backgroundColor = (0, 0, 0), printFps = False): #MApp's constructor
+        self._type = "MApp"
+
+        MWidget.__init__(self, 0, 0, windowWidth, windowHeight, 0, backgroundColor) #Parent class constructor call
+        self.deltaTime = 0
+        self.fps = 0
+        self.printFps = printFps
+        self.setWindowTitle(windowTitle)
+        self._deltaTimeCache = time_ns()
+        self._fpsCount = 0
+        self._fpsDuration = 0
         self._pygameWindow = pygameWindow
 
-    def frame(self):
+    def frame(self): #Do a frame in the application
         self.frameEvent()
+        self.frameGraphics()
 
-    def frameEvent(self):
+    def frameEvent(self): #Do all events updates in the application
+        self.deltaTime = (time_ns() - self._deltaTimeCache)/(10**9)
+        self._deltaTimeCache = time_ns()
+
+        self._fpsDuration += self.deltaTime
+        if self._fpsDuration >= 1:
+            self.fps = self._fpsCount
+            self._fpsDuration -= 1
+            self._fpsCount = 0
+
+            if self.printFps:
+                print(self.fps)
+
+        self._fpsCount += 1
+
         events = pygame.event.get()
-        for event in events:
+        for event in events: #Event handler
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+
+    def frameGraphics(self): #Do all graphics updates in the application
+        self._pygameWindow.blit(self._render(), (0, 0, self.width, self.height))
+
+    def getDeltaTime(self): #Return deltaTime
+        return self.deltaTime
+    
+    def getFps(self): #Return fps
+        return self.fps
+    
+    def getPrintFps(self): #Return printFps
+        return self.printFps
+    
+    def setPrintFps(self, printFps): #Change the value of printFps
+        self.printFps = printFps
+
+    def setWindowTitle(self, windowTitle): #Change the title of the window
+        self.windowTitle = windowTitle
+
+        pygame.display.set_caption(windowTitle)
