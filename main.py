6f746097c19a98a87d867344ed7ece37b89639a3
Mattool81974 <88587164+Mattool81974@users.noreply.github.com>
2023-09-07 20:07:42 +0200

    MLib 1.2.0

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint
 from time import sleep
 
 #TAILLE = (700, 394)
-TAILLE = (1000, 1000)
+TAILLE = (900, 900)
 
 fenetre = display.set_mode(TAILLE)
 mapp = MApp(fenetre, "Test", TAILLE[0], TAILLE[1], printFps=True)
@@ -15,26 +15,33 @@ phrase = "Le T-90 est un char de combat russe associant le char T-72B et certain
 nb = 0
 temps = 0
 
-image1 = MImage("C:\\Users\\there\\Pictures\\donaldTrump.png", 0, 0, 500, 500, mapp)
-image1.setFrameWidth(3)
-image1.setImageHorizontalAlignment(0)
-image1.setImageReframing(1)
-image1.setImageSize((250, 250))
+textes = []
 
-image2 = MImage("C:\\Users\\there\\Pictures\\donaldTrump.png", 500, 0, 500, 500, mapp)
-image2.setFrameWidth(3)
-image2.setImageReframing(2)
-image2.setImageSize((250, 250))
+for i in range(3):
+    texte = MText(phrase, 100, 300 * i, 300, 300, mapp)
+    texte.setCursorVisible(True)
+    texte.setFontSize(22)
+    texte.setFrameColor((255, 0, 0))
+    texte.setFrameWidth(2)
+    texte.setFrameWidth(20, 2)
+    texte.setInput(True)
+    texte.setTextVerticalAlignment(i)
+    texte.setTextOffset(10, 0)
+    texte.setTextOffset(40, 2)
 
-image3 = MImage("C:\\Users\\there\\Pictures\\donaldTrump.png", 0, 500, 500, 500, mapp)
-image3.setFrameWidth(3)
-image3.setImageReframing(3)
-image3.setImageSize((250, 250))
+    texte2 = MText(phrase, 500, 300 * i, 300, 300, mapp)
+    texte2.setCursorVisible(True)
+    texte2.setDynamicTextCut(True)
+    texte2.setFontSize(22)
+    texte2.setFrameColor((255, 0, 0))
+    texte2.setFrameWidth(2)
+    texte2.setFrameWidth(20, 2)
+    texte2.setInput(True)
+    texte2.setTextVerticalAlignment(i)
+    texte2.setTextOffset(10, 0)
+    texte2.setTextOffset(40, 2)
 
-image4 = MImage("C:\\Users\\there\\Pictures\\donaldTrump.png", 500, 500, 500, 500, mapp)
-image4.setFrameWidth(3)
-image4.setImageReframing(4)
-image4.setImageSize((250, 250))
+    textes.append(texte)
 
 while True:
     mapp.frameEvent()
